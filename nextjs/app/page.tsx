7e9c6b902bf37c12dbab70f4a062710c0949e455
Pavan Kumar <siriyalapavankumar@gmail.com>
2025-08-19 18:58:01 +0530

    nextjs day-1

---
+++ nextjs/app/page.tsx
@@ -0,0 +1,9 @@
+export default function Home() {
+  return (
+    <div  className="flex flex-col items-center justify-center h-screen bg-zinc-800">
+   <h1 className="text-5xl font-bold text-yellow-400">
+    Hello world
+   </h1>
+    </div>
+  )
+}
